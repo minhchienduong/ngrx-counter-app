54df5e6ef3440f528853f56eeef9c896329ee2dd
Minh Chien DUONG <chienduong90@gmail.com>
2024-06-22 21:51:13 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+
+import { AppComponent } from './app.component';
+import { CounterOutputComponent } from './counter-output/counter-output.component';
+import { CounterControlsComponent } from './counter-controls/counter-controls.component';
+import { counterReducer } from './store/counter.reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { CounterEffects } from './store/counter.effects';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CounterOutputComponent,
+    CounterControlsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({
+      counter: counterReducer,
+    }),
+    EffectsModule.forRoot([CounterEffects]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
